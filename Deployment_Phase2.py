dbb7d47d3b6945d957bbc7381d3060dfb5198751
sumathipriya <111436606+sumathipriya@users.noreply.github.com>
2023-08-04 11:33:54 +0530

    Delete Deployment_Phase2.py

--- Deployment_Phase2.py
+++
@@ -1,6 +0,0 @@
-import  pickle
-#Deployement phase
-loaded_model = pickle.load(open("finalized_model_linear.sav",'rb'))
-#0 is origin or initial value or bias value,its the intial salary 
-result =  loaded_model.predict([[0]])
-print(result)
